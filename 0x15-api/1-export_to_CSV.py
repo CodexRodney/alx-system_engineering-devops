0ab918fce1dcb295091b43fa12a60f134c2a5a04
CodexRodney <rodney002mwanje@gmail.com>
2022-06-17 00:54:16 +0300

    Made code not executable when imported

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,36 +9,38 @@ in csv format
 
 import csv
 import json
-import urllib.request
 import sys
+import urllib.request
 
 
-url = "https://jsonplaceholder.typicode.com/todos"
-req = urllib.request.Request(url)
-user_id = sys.argv[1]
-file_name = "{}.csv".format(user_id)
-new_list = []  # contains dictionaries to convert to csv
-req_2 = "https://jsonplaceholder.typicode.com/users/{}" \
-    .format(user_id)
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/todos"
+    req = urllib.request.Request(url)
+    user_id = sys.argv[1]
+    file_name = "{}.csv".format(user_id)
+    new_list = []  # contains dictionaries to convert to csv
+    req_2 = "https://jsonplaceholder.typicode.com/users/{}" \
+        .format(user_id)
 
-with urllib.request.urlopen(req_2) as response:
-    character_set = response.headers.get_content_charset()
-    body = json.loads(response.read().decode(character_set))
-    user_name = body["username"]
+    with urllib.request.urlopen(req_2) as response:
+        character_set = response.headers.get_content_charset()
+        body = json.loads(response.read().decode(character_set))
+        user_name = body["username"]
 
-with urllib.request.urlopen(req) as response:
-    character_set_2 = response.headers.get_content_charset()
-    body_2 = json.loads(response.read().decode(character_set_2))
-    for i in body_2:
-        new_dict = {}
-        if i["userId"] == int(user_id):
-            new_dict["userId"] = i["userId"]
-            new_dict["username"] = user_name
-            new_dict["completed"] = i["completed"]
-            new_dict["title"] = i["title"]
-            new_list.append(new_dict)
+    with urllib.request.urlopen(req) as response:
+        character_set_2 = response.headers.get_content_charset()
+        body_2 = json.loads(response.read().decode(character_set_2))
+        for i in body_2:
+            new_dict = {}
+            if i["userId"] == int(user_id):
+                new_dict["userId"] = i["userId"]
+                new_dict["username"] = user_name
+                new_dict["completed"] = i["completed"]
+                new_dict["title"] = i["title"]
+                new_list.append(new_dict)
 
-new_dict_keys = ["userId", "username", "completed", "title"]
-with open(file_name, 'w') as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=new_dict_keys)
-    writer.writerows(new_list)
+    new_dict_keys = ["userId", "username", "completed", "title"]
+    with open(file_name, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=new_dict_keys)
+        writer.writerows(new_list)
+
